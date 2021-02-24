bc7a80a0e61d34a341decf008d06f67f68d2de13
12613 <1261337209@qq.com>
2021-02-25 00:44:21 +0800

    0225

--- src/com/king/D0224/Text2.java
+++ src/com/king/D0224/Text2.java
@@ -1,5 +1,7 @@
 package com.king.D0224;
 
+import java.util.Arrays;
+
 /**
  * @program: leetcode
  * @description:
@@ -7,4 +9,26 @@ package com.king.D0224;
  * @create: 2021-02-25 00:26
  */
 public class Text2 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] a = solution.transpose(matrix);
+
+        System.out.println(Arrays.deepToString(a));
+    }
+}
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                arr[j][i] = matrix[i][j];
+            }
+        }
+
+        return arr;
+    }
 }
