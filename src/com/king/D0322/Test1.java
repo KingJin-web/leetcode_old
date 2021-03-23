a5954e329ae859ed1974cb3b435ccc0939c006af
12613 <1261337209@qq.com>
2021-03-23 21:28:55 +0800

    整数反转

--- src/com/king/D0322/Test1.java
+++ src/com/king/D0322/Test1.java
@@ -7,4 +7,30 @@ package com.king.D0322;
  * @create: 2021-03-23 20:59
  */
 public class Test1 {
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        System.out.println(test1.reverse(1234602100));
+    }
+
+    public int reverse(int x) {
+        long n = 0;
+        while(x != 0) {
+            n = n*10 + x%10;
+            x = x/10;
+        }
+        return (int)n==n? (int)n:0;
+    }
+
+    public int reverse2(int x) {
+        int ans = 0;
+        while (x != 0) {
+            if ((ans * 10) / 10 != ans) {
+                ans = 0;
+                break;
+            }
+            ans = ans * 10 + x % 10;
+            x = x / 10;
+        }
+        return ans;
+    }
 }
