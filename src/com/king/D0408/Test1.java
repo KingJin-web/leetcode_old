3d381c2cd3e7230ece18a59a914d5b22b6578bd8
12613 <1261337209@qq.com>
2021-04-08 20:28:08 +0800

    153. 寻找旋转排序数组中的最小值

--- src/com/king/D0408/Test1.java
+++ src/com/king/D0408/Test1.java
@@ -1,5 +1,7 @@
 package com.king.D0408;
 
+import java.util.Date;
+
 /**
  * @program: leetcode
  * @description:
@@ -7,4 +9,51 @@ package com.king.D0408;
  * @create: 2021-04-08 19:05
  */
 public class Test1 {
+    /**
+     * 循环查找
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：37.7 MB, 在所有 Java 提交中击败了85.54%的用户
+     *
+     * @param nums
+     * @return
+     */
+    public int findMin1(int[] nums) {
+        if (nums.length <= 1) {
+            return nums[0];
+        }
+        int min = nums[0];
+        for (int i : nums) {
+            if (min > i) {
+                min = i;
+            }
+        }
+        return min;
+    }
+
+    public int findMin2(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        int pivot;
+        while (left < right) {
+            pivot = left + (right - left) / 2;
+            if (nums[pivot] < nums[right]) {
+                right = pivot;
+            } else {
+                left = pivot + 1;
+            }
+        }
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = new int[]{3, 4, 5, 6, 1, 2};
+        Date date = new Date();
+        long date1 = date.getTime();
+        int a = new Test1().findMin1(arr);
+        long date2 = date.getTime();
+        int b = new Test1().findMin2(arr);
+        long date3 = date.getTime();
+
+        System.out.println((date2 - date1) + " " + (date3 - date2) + "\n " + a + " " + b);
+    }
 }
---
+++ src/com/king/D0408/Test2.java
@@ -0,0 +1,7 @@
+package com.king.D0408;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+    }
+}
