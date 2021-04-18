9300bbf425d93fb32663c82e45d4a007eb6e4e3c
12613 <1261337209@qq.com>
2021-04-18 14:24:12 +0800

    快速排序

--- src/com/king/tencent/arr_and_str/Test2.java
+++ src/com/king/tencent/arr_and_str/Test2.java
@@ -10,15 +10,28 @@ import java.util.Arrays;
  * @create: 2021-04-17 15:37
  */
 public class Test2 {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2){
+    /**
+     * 通过
+     * 显示详情
+     * 执行用时：14 ms, 在所有 Java 提交中击败了7.26% 的用户
+     * 内存消耗：39.3 MB, 在所有 Java 提交中击败了97.98% 的用户
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
         int[] arr = concat(nums1, nums2);
-        arr = sort(arr);
+
+        sort(arr);
+
+        int a = arr.length / 2;
         if (arr.length % 2 == 0) {
-            int n = arr.length / 2;
-            return (double) (arr[n] + arr[n - 1]) / 2;
+
+            return (double) (arr[a] + arr[a - 1]) / 2;
         } else {
-            int n = arr.length / 2;
-            return (double) arr[n];
+            return (double) arr[a];
         }
 
     }
@@ -30,7 +43,7 @@ public class Test2 {
         return c;
     }
 
-    public int[] sort(int[] sourceArray){
+    public int[] sort(int[] sourceArray) {
         // 对 arr 进行拷贝，不改变参数内容
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
 
@@ -65,12 +78,59 @@ public class Test2 {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+
+
     public static void main(String[] args) {
-        int[] nums1 = {1, 5}, nums2 = {3, 5, 4};
+        int[] nums1 = {}, nums2 = {3, 5, 4};
         Test2 t2 = new Test2();
-        int[] arr = concat(nums1, nums2);
-        System.out.println(Arrays.toString(arr));
-        t2.findMedianSortedArrays(nums1, nums2);
+
+        System.out.println(t2.findMedianSortedArrays(nums1, nums2));
+    }
+
+    /**
+     * 执行用时：5 ms, 在所有 Java 提交中击败了13.45% 的用户
+     * 内存消耗：39.3 MB, 在所有 Java 提交中击败了97.06% 的用户
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] arr = new int[n + m];
+        for (int i = 0; i < m; i++) {
+            arr[i] = nums1[i];
+        }
+        arrMerge(arr, m, nums2, n);
+        int a = arr.length / 2;
+        if (arr.length % 2 == 0) {
+
+            return (double) (arr[a] + arr[a - 1]) / 2;
+        } else {
+            return (double) arr[a];
+        }
+
+    }
+
+    private void arrMerge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int a = m + n - 1;
+        //判断两个数组，进行遍历比较
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[a--] = nums1[i--];
+            } else {
+                nums1[a--] = nums2[j--];
+            }
+        }
+        //如果nums1遍历提前结束，将nums2剩下的所有数字直接给nums1
+        if (i < 0) {
+            for (int b = j; b >= 0; b--) {
+                nums1[a--] = nums2[b];
+            }
+        }
+        //如果nums2遍历提前结束，此时nums1已经排序结束，无需今进行下一步操作
     }
 
 
