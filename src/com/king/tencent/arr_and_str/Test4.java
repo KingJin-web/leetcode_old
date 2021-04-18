7f97ae6a4c16595a0fc72b83d7dcbe51777f5dd4
12613 <1261337209@qq.com>
2021-04-18 18:05:37 +0800

    字符串转换整数 (atoi)

--- src/com/king/tencent/arr_and_str/Test4.java
+++ src/com/king/tencent/arr_and_str/Test4.java
@@ -27,7 +27,8 @@ public class Test4 {
         int flag = 1;
         long sum = 0;
 
-        for (int i = 0; i < ch.length; i++) {
+        for (int i = 0; i < ch.length
+                ; i++) {
             if (ch[i] == '-' && i == 0) {
                 flag = -1;
                 continue;
