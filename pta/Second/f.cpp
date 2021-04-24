5809fdcf51cb019ba1f5c296bf848f2f7454174f
12613 <1261337209@qq.com>
2021-04-24 22:15:15 +0800

    377. 组合总和 Ⅳ

--- pta/Second/f.cpp
+++ pta/Second/f.cpp
@@ -1,50 +1,42 @@
-//7 - 4 Split(20 分)
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-int main(int argc, char const *argv[])
+int n;
+int a[1555555];
+int b[1555555];
+void check(int l, int r)
+{
+    if (l == r)
+        return;
+    int n = 0;
+    for (int i = l; i <= r; i += 2)
+        b[++n] = a[i];
+    for (int i = l + 1; i <= r; i += 2)
+        b[++n] = a[i];
+    for (int i = l; i <= r; i++)
+    {
+        a[i] = b[i - l + 1];
+    }
+    int mid = (l + r) >> 1;
+    check(l, mid);
+    check(mid + 1, r);
+}
+int main()
 {
     int n;
     cin >> n;
-    int arr[n];
-    int arr2[n];
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        cin >> arr[i];
+        scanf("%d", &a[i]);
     }
-
-    double a = n;
-    for (int i = 1; i <= n; i *= 2)
+    check(1, n);
+    bool flag = false;
+    for (int i = 1; i <= n; i++)
     {
-        /* code */
-        int c = n / 2;
-        for (int i = 0; i < n; i++)
-        {
-            if (i % 2 == 0)
-            {
-                arr2[i / 2] = arr[i];
-            }
-            else
-            {
-                arr2[c] = arr[i];
-                c++;
-            }
-        }
-        for (int i = 0; i < n; ++i)
-        {
-            arr[i] = arr2[i];
-        }
-        for (int i = 0; i < n; ++i)
-        {
-            cout << arr[i];
-        }
-        cout << endl;
-        a = a / 2;
-    }
-
-    for (int i = 0; i < n; ++i)
-    {
-        cout << arr[i] << endl;
+        if (flag)
+            printf(" ");
+        flag = true;
+        printf("%d", a[i]);
     }
     return 0;
 }
+
