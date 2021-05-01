86c9e35634508e8002f3bdf2b568ef9e4154899a
12613 <1261337209@qq.com>
2021-05-01 18:07:43 +0800

    690. 员工的重要性

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # leetcode 刷题
 
-## [java实验课](src/com/king/MyJava)
--- src/com/king/M05/Test1.java
+++ src/com/king/M05/Test1.java
@@ -1,24 +1,42 @@
 package com.king.M05;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program: leetcode
  * @description: 690. 员工的重要性
+ * https://leetcode-cn.com/problems/employee-importance/submissions/
  * @author: King
  * @create: 2021-05-01 10:23
  */
 public class Test1 {
 
-    public static void main(String[] args) {
-        int a = 1000;
-        Integer a1 = new Integer(1000) ;
-        Integer b1 = new Integer(1000);
-        System.out.println(a1 == b1);
+    Map<Integer, Employee> map = new HashMap<Integer, Employee>();
 
-        Integer a2 = 100;
-        Integer b2 = 100;
-        System.out.println(a2 == b2);
+    /**
+     * 执行用时：5 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：39.6 MB, 在所有 Java 提交中击败了91.61%的用户
+     * @param employees
+     * @param id
+     * @return
+     */
+    public int getImportance(List<Employee> employees, int id) {
+        for (Employee employee : employees) {
+            map.put(employee.id, employee);
+        }
+        return dfs(id);
+    }
+
+    public int dfs(int id) {
+        Employee employee = map.get(id);
+        int total = employee.importance;
+        List<Integer> subordinates = employee.subordinates;
+        for (int subId : subordinates) {
+            total += dfs(subId);
+        }
+        return total;
     }
 }
 
