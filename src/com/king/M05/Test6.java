9bc5897db7b39ee3e45fd10c5c3db7b776fa9349
12613 <1261337209@qq.com>
2021-05-04 13:46:20 +0800

    1480. 一维数组的动态和

---
+++ src/com/king/M05/Test6.java
@@ -0,0 +1,11 @@
+package com.king.M05;
+
+/**
+ * @program: leetcode
+ * @description: 1480. 一维数组的动态和
+ * https://leetcode-cn.com/problems/running-sum-of-1d-array/
+ * @author: King
+ * @create: 2021-05-04 13:43
+ */
+public class Test6 {
+}
