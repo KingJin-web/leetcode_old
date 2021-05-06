1a662881449cafba3410e44c71456c276675c205
12613 <1261337209@qq.com>
2021-05-06 22:06:08 +0800

    1720. 解码异或后的数组

---
+++ src/com/king/M05/Test8.java
@@ -0,0 +1,20 @@
+package com.king.M05;
+
+/**
+ * @program: leetcode
+ * @description: 1720. 解码异或后的数组
+ * https://leetcode-cn.com/problems/decode-xored-array/
+ * @author: King
+ * @create: 2021-05-06 22:04
+ */
+public class Test8 {
+    public int[] decode(int[] encoded, int first) {
+        int n = encoded.length + 1;
+        int[] arr = new int[n];
+        arr[0] = first;
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i - 1] ^ encoded[i - 1];
+        }
+        return arr;
+    }
+}
