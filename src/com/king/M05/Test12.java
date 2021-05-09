2602ebee1560ce246cb2f4497fee97f61e29f9de
12613 <1261337209@qq.com>
2021-05-10 00:17:31 +0800

    java 考试题

---
+++ src/com/king/M05/Test12.java
@@ -0,0 +1,11 @@
+package com.king.M05;
+
+/**
+ * @program: leetcode
+ * @description: 1482. 制作 m 束花所需的最少天数
+ * https://leetcode-cn.com/problems/minimum-number-of-days-to-make-m-bouquets/
+ * @author: King
+ * @create: 2021-05-10 00:15
+ */
+public class Test12 {
+}
