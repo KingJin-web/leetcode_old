78c06144777f5ea911db1dd7d5f247b9b2f5195c
12613 <1261337209@qq.com>
2021-05-18 00:52:27 +0800

    1828. 统计一个圆中点的数目

---
+++ src/com/king/M05/Test20.java
@@ -0,0 +1,11 @@
+package com.king.M05;
+
+/**
+ * @program: leetcode
+ * @description: 1828. 统计一个圆中点的数目
+ * https://leetcode-cn.com/problems/queries-on-number-of-points-inside-a-circle/
+ * @author: King
+ * @create: 2021-05-18 00:48
+ */
+public class Test20 {
+}
