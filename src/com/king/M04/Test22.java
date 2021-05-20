7ff4f681a5b817086cfd3ec39947ae95243ff723
12613 <1261337209@qq.com>
2021-05-21 00:17:58 +0800

    692. 前K个高频单词

--- src/com/king/M04/Test22.java
+++ src/com/king/M04/Test22.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 /**
  * @program: leetcode
+ * https://leetcode-cn.com/problems/top-k-frequent-words/
  * @description: 692. 前K个高频单词
  * @author: King
  * @create: 2021-05-20 23:37
@@ -41,6 +42,56 @@ public class Test22 {
         test22.topKFrequent(arr,3);
     }
 
+    class Solution {
+        public List<String> topKFrequent(String[] words, int k) {
+            Map<String, Integer> cnt = new HashMap<String, Integer>();
+            for (String word : words) {
+                cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+            }
+            PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<Map.Entry<String, Integer>>(new Comparator<Map.Entry<String, Integer>>() {
+                public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+                    return entry1.getValue() == entry2.getValue() ? entry2.getKey().compareTo(entry1.getKey()) : entry1.getValue() - entry2.getValue();
+                }
+            });
+            for (Map.Entry<String, Integer> entry : cnt.entrySet()) {
+                pq.offer(entry);
+                if (pq.size() > k) {
+                    pq.poll();
+                }
+            }
+            List<String> ret = new ArrayList<String>();
+            while (!pq.isEmpty()) {
+                ret.add(pq.poll().getKey());
+            }
+            Collections.reverse(ret);
+            return ret;
+        }
+    }
+
+
+        public List<String> topKFrequent1(String[] words, int k) {
+            Map<String, Integer> cnt = new HashMap<String, Integer>();
+            for (String word : words) {
+                cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+            }
+            PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<Map.Entry<String, Integer>>(new Comparator<Map.Entry<String, Integer>>() {
+                public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+                    return entry1.getValue() == entry2.getValue() ? entry2.getKey().compareTo(entry1.getKey()) : entry1.getValue() - entry2.getValue();
+                }
+            });
+            for (Map.Entry<String, Integer> entry : cnt.entrySet()) {
+                pq.offer(entry);
+                if (pq.size() > k) {
+                    pq.poll();
+                }
+            }
+            List<String> ret = new ArrayList<String>();
+            while (!pq.isEmpty()) {
+                ret.add(pq.poll().getKey());
+            }
+            Collections.reverse(ret);
+            return ret;
+        }
 
 
 }
