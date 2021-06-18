9da8185fb2e243c6598784da40a0e13bc22554a8
12613 <1261337209@qq.com>
2021-06-18 23:43:54 +0800

    877. 石子游戏

---
+++ src/com/king/M06/Test6.java
@@ -0,0 +1,12 @@
+package com.king.M06;
+
+/**
+ * @program: leetcode
+ * @description: 877. 石子游戏
+ * https://leetcode-cn.com/problems/stone-game/
+ * @author: King
+ * @create: 2021-06-18 23:00
+ */
+public class Test6 {
+
+}
