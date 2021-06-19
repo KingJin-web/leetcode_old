0736997be49f22b368a6b10b9b30dcf628af6773
12613 <1261337209@qq.com>
2021-06-19 23:47:16 +0800

    1239. 串联字符串的最大长度

--- src/com/king/M06/Test7.java
+++ src/com/king/M06/Test7.java
@@ -2,7 +2,7 @@ package com.king.M06;
 
 /**
  * @program: leetcode
- * @description:     }         ans = Math.max(ans, builder.length());         int size = arr.size();         for (int i = start; i < size; i++) {             String s = arr.get(i);             builder.append(s);             dfs(arr, i + 1, builder);             builder.delete(builder.length() - s.length(), builder.length());         }     }      private boolean match(StringBuilder builder) {         String s = builder.toString();         int[] counter = new int[26];         for (int i = 0; i < s.length(); i++) {             char c = s.charAt(i);             counter[c - 97]++;             if (counter[c - 97] > 1) {                 return false;             }         }         return true;     }
+ * @description:     1239. 串联字符串的最大长度
  * @author: King
  * @create: 2021-06-19 23:30
  */
