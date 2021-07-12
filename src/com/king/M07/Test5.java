9bb1e055e3deb5223e598a914be2ae9cae058891
12613 <1261337209@qq.com>
2021-07-12 23:24:20 +0800

    😂 1. 两数之和

---
+++ src/com/king/M07/Test5.java
@@ -0,0 +1,15 @@
+package com.king.M07;
+
+/**
+ * @program: leetcode
+ * @description: 1. 两数之和
+ * https://leetcode-cn.com/problems/two-sum/
+ * @author: King
+ * @create: 2021-07-12 23:09
+ */
+public class Test5 {
+    public int[] twoSum(int[] nums, int target) {
+
+
+    }
+}
