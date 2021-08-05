b9a63442310829a5d87147e4ad8601763c37a920
12613 <1261337209@qq.com>
2021-08-05 23:04:18 +0800

    ✔  560. 和为K的子数组

--- src/com/king/M08/Test4.java
+++ src/com/king/M08/Test4.java
@@ -40,6 +40,3 @@ public class Test4 {
     }
 
 }
-abstract class a{
-
-}
---
+++ src/com/king/M08/Test5.java
@@ -0,0 +1,17 @@
+package com.king.M08;
+
+/**
+ * @program: leetcode
+ * @description:
+ * @author: King
+ * @create: 2021-08-05 23:02
+ */
+public class Test5 {
+    /**
+     * 560. 和为K的子数组
+     * https://leetcode-cn.com/problems/subarray-sum-equals-k/
+     */
+    public static class T1{
+
+    }
+}
