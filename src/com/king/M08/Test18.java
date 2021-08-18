a42c4c4de1eb06e9b99256380164d8b7893f3938
KingJin-web <1261337209@qq.com>
2021-08-18 23:21:01 +0800

    ✔ 58. 最后一个单词的长度

---
+++ src/com/king/M08/Test18.java
@@ -0,0 +1,14 @@
+package com.king.M08;
+
+/**
+ * @program: leetcode
+ * @description: 58. 最后一个单词的长度
+ * 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中最后一个单词的长度。
+ * <p>
+ * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+ * @author: King
+ * @create: 2021-08-18 23:18
+ */
+public class Test18 {
+
+}
