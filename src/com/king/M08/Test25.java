8b0def8507ef780b72c24c3e2b0128ba7d1e69fd
KingJin-web <1261337209@qq.com>
2021-08-29 23:17:53 +0800

    ✔ 1588. 所有奇数长度子数组的和

--- src/com/king/M08/Test25.java
+++ src/com/king/M08/Test25.java
@@ -1,10 +1,41 @@
 package com.king.M08;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @program: leetcode
- * @description:
+ * @description: 剑指 Offer II 042. 最近请求次数
+ * https://leetcode-cn.com/problems/H8086Q/
  * @author: King
- * @create: 2021-08-28 23:04
+ * @create: 2021-08-25 23:04
  */
 public class Test25 {
+    private final RecentCounter p = new RecentCounter();
+
+    public static class RecentCounter {
+
+        Queue<Integer> queue;
+
+        public RecentCounter() {
+            queue = new LinkedList<>();
+        }
+
+        public int ping(int t) {
+            queue.offer(t);
+
+            while (queue.peek() < t - 3000) {
+                queue.poll();
+            }
+
+            return queue.size();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Test25 test25 = new Test25();
+        test25.p.ping(155);
+    }
 }
+
