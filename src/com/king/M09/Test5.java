f740114cf0cb1711ec58f45b625b141bcf6ec59b
KingJin-web <1261337209@qq.com>
2021-09-09 13:31:27 +0800

    ✔ 470. 用 Rand7() 实现 Rand10()

--- src/com/king/M09/Test5.java
+++ src/com/king/M09/Test5.java
@@ -1,5 +1,11 @@
 package com.king.M09;
 
+import com.king.Helper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * @program: leetcode
  * @description: 470. 用 Rand7() 实现 Rand10()
@@ -12,6 +18,34 @@ package com.king.M09;
 public class Test5 {
     //已有方法rand7可生成 1 到 7 范围内的均匀随机整数，试写一个方法?rand10生成 1 到 10 范围内的均匀随机整数。
     //不要使用系统的Math.random()方法。
+    public int rand10() {
+        int ans = rand2();
+        for (int i = 0; i < 3; i++) {
+            ans <<= 1;
+            ans ^= rand2();
+        }
+        return (ans <= 10 && ans > 0) ? ans : rand10();
+    }
+
+    public int rand2() {
+        int ans = rand7();
+        return ans == 7 ? rand2() : ans % 2;
+    }
+
+    private int rand7() {
+        Random random = new Random();
+        return random.nextInt(7) + 1;
+    }
+
+    public static void main(String[] args) {
+
+       Test5 test5 = new Test5();
+       List<Integer> list = new ArrayList<>();
+       while (list.size() < 1000){
+           list.add(test5.rand10());
+       }
+       Helper.print(list.toArray(),5);
+    }
 
 
 }
