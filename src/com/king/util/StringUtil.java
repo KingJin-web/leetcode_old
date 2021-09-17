42374eda2f87e06ea81a6052588be423c4d54a8f
KingJin-web <1261337209@qq.com>
2021-09-17 20:53:25 +0800

    ✔ StringUtil

--- src/com/king/util/StringUtil.java
+++ src/com/king/util/StringUtil.java
@@ -13,14 +13,14 @@ public class StringUtil {
 
 
     //检测字符串中是否有中文  不包含中文符号
-    public static boolean isContainChinese1(String str) {
+    public static boolean isContainChinese(String str) {
         Pattern p = Pattern.compile("[\u4e00-\u9fa5]+");
         Matcher m = p.matcher(str);
         return m.find();
     }
 
     //检测字符串中是否有双字节字符包含中文,中文符号
-    public static boolean isContainChinese2(String str) {
+    public static boolean isContainTwoByte(String str) {
         Pattern p = Pattern.compile("[^\\x00-\\xff]");
         Matcher m = p.matcher(str);
         return m.find();
