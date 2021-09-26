f1d959c9b1746964c697fc9473154ad6a96b8f9a
KingJin-web <1261337209@qq.com>
2021-09-26 23:07:10 +0800

    ✔ 371. 两整数之和

--- src/com/king/M09/Test26.java
+++ src/com/king/M09/Test26.java
@@ -1,5 +1,8 @@
 package com.king.M09;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 /**
  * @program: leetcode
  * @description: 371. 两整数之和
@@ -9,4 +12,19 @@ package com.king.M09;
  * @create: 2021-09-26 22:52
  */
 public class Test26 {
+
+    //执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗： 35.3 MB , 在所有 Java 提交中击败了 17.26% 的用户
+    public int getSum1(int a, int b) {
+        return a + b;
+    }
+
+    //执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗： 35 MB , 在所有 Java 提交中击败了 84.83% 的用户
+    public int getSum(int a,int b){
+        int [] arr = {a,b};
+        return Arrays.stream(arr).sum();
+    }
+
+    public int getSum2(int a, int b) {
+        return BigInteger.valueOf(a).add(BigInteger.valueOf(b)).intValue();
+    }
 }
