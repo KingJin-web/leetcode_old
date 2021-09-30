a9a54c4d3146864e6aec4c3d5a634c52c74bd4e3
KingJin-web <1261337209@qq.com>
2021-09-30 23:27:49 +0800

    ✔ 223. 矩形面积

---
+++ src/com/king/M09/Test28.java
@@ -0,0 +1,18 @@
+package com.king.M09;
+
+/**
+ * @program: leetcode
+ * @description: 223. ¾ØÐÎÃæ»ý
+ * https://leetcode-cn.com/problems/rectangle-area/
+ * @author: King
+ * @create: 2021-09-30 23:24
+ */
+public class Test28 {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1), area2 = (bx2 - bx1) * (by2 - by1);
+        int overlapWidth = Math.min(ax2, bx2) - Math.max(ax1, bx1), overlapHeight = Math.min(ay2, by2) - Math.max(ay1, by1);
+        int overlapArea = Math.max(overlapWidth, 0) * Math.max(overlapHeight, 0);
+        return area1 + area2 - overlapArea;
+    }
+
+}
