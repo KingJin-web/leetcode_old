a409e83b4d0c95065c14a302d0f2a27079a839d4
KingJin-web <1261337209@qq.com>
2021-10-20 23:35:37 +0800

    ✔ 453. 最小操作次数使数组元素相等

--- src/com/king/M10/Test20.java
+++ src/com/king/M10/Test20.java
@@ -3,6 +3,9 @@ package com.king.M10;
 /**
  * @program: leetcode
  * @description: 453. 最小操作次数使数组元素相等
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @author: King
  * @create: 2021-10-20 23:14
  */
@@ -34,4 +37,14 @@ public class Test20 {
         }
         return res;
     }
+
+
+    //给你一个长度为 n 的整数数组，每次操作将会使 n - 1 个元素增加 1 。返回让数组所有元素相等的最小操作次数。
+    //输入：nums = [1,2,3]
+    //输出：3
+    //解释：
+    //只需要3次操作（注意每次操作会增加两个元素的值）：
+    //[1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+
+
 }
