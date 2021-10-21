8048496e7ff13c62b01f892a985d9348aa955205
KingJin-web <1261337209@qq.com>
2021-10-21 21:02:05 +0800

    ✔ 66. 加一

--- src/com/king/M10/Test21.java
+++ src/com/king/M10/Test21.java
@@ -36,7 +36,23 @@ public class Test21 {
         }
         return digits;
     }
+    public int[] plusOne1(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] != 9) {
+                ++digits[i];
+                for (int j = i + 1; j < n; ++j) {
+                    digits[j] = 0;
+                }
+                return digits;
+            }
+        }
 
+        // digits 中所有的元素均为 9
+        int[] ans = new int[n + 1];
+        ans[0] = 1;
+        return ans;
+    }
     public static void main(String[] args) {
         Test21 test21 = new Test21();
         MyPrint.print(test21.plusOne(Helper.getArrays(8, 8, 9, 9)));
