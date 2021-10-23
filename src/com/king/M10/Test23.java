631a07ec7cf5058a53f61e20c9e7c871e5ad8b8f
KingJin-web <1261337209@qq.com>
2021-10-24 02:22:06 +0800

    ✔  492. 构造矩形

--- src/com/king/M10/Test23.java
+++ src/com/king/M10/Test23.java
@@ -9,7 +9,7 @@ import com.king.util.MyPrint;
  * @author: King
  * @create: 2021-10-23 23:11
  */
-public class Test33 {
+public class Test23 {
 
     //作为一位web开发者， 懂得怎样去规划一个页面的尺寸是很重要的。 现给定一个具体的矩形页面面积，
     // 你的任务是设计一个长度为 L 和宽度为 W 且满足以下要求的矩形的页面。要求：
@@ -22,7 +22,7 @@ public class Test33 {
 
     //执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
     //内存消耗： 35.8 MB , 在所有 Java 提交中击败了 59.59% 的用户
-    public int[] constructRectangle(int area) {
+    public int[] constructRectangle1(int area) {
         int w = (int) Math.sqrt(area);
         while (area % w != 0) {
             --w;
@@ -30,8 +30,17 @@ public class Test33 {
         return new int[]{area / w, w};
     }
 
+
+    //根据题意，从 sqrt(area) 开始往后模拟，遇到第一个能够被整除的数值，则返回该答案。
+        public int[] constructRectangle(int area) {
+            for (int i = (int)(Math.sqrt(area)); ;i--) {
+                if (area % i == 0) return new int[]{area / i, i};
+            }
+        }
+
+
     public static void main(String[] args) {
-        Test33 test33 = new Test33();
+        Test23 test33 = new Test23();
         MyPrint.print(test33.constructRectangle(11));
     }
 }
---
+++ src/com/king/M10/Test24.java
@@ -0,0 +1,60 @@
+package com.king.M10;
+
+import java.util.*;
+/**
+ * @program: leetcode
+ * @description: 638. 大礼包
+ * @author: King
+ * @create: 2021-10-24 02:20
+ */
+public class Test24 {
+    Map<List<Integer>, Integer> memo = new HashMap<List<Integer>, Integer>();
+
+    public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        int n = price.size();
+
+        // 过滤不需要计算的大礼包，只保留需要计算的大礼包
+        List<List<Integer>> filterSpecial = new ArrayList<List<Integer>>();
+        for (List<Integer> sp : special) {
+            int totalCount = 0, totalPrice = 0;
+            for (int i = 0; i < n; ++i) {
+                totalCount += sp.get(i);
+                totalPrice += sp.get(i) * price.get(i);
+            }
+            if (totalCount > 0 && totalPrice > sp.get(n)) {
+                filterSpecial.add(sp);
+            }
+        }
+
+        return dfs(price, special, needs, filterSpecial, n);
+    }
+
+    // 记忆化搜索计算满足购物清单所需花费的最低价格
+    public int dfs(List<Integer> price, List<List<Integer>> special, List<Integer> curNeeds, List<List<Integer>> filterSpecial, int n) {
+        if (!memo.containsKey(curNeeds)) {
+            int minPrice = 0;
+            for (int i = 0; i < n; ++i) {
+                minPrice += curNeeds.get(i) * price.get(i); // 不购买任何大礼包，原价购买购物清单中的所有物品
+            }
+            for (List<Integer> curSpecial : filterSpecial) {
+                int specialPrice = curSpecial.get(n);
+                List<Integer> nxtNeeds = new ArrayList<Integer>();
+                for (int i = 0; i < n; ++i) {
+                    if (curSpecial.get(i) > curNeeds.get(i)) { // 不能购买超出购物清单指定数量的物品
+                        break;
+                    }
+                    nxtNeeds.add(curNeeds.get(i) - curSpecial.get(i));
+                }
+                if (nxtNeeds.size() == n) { // 大礼包可以购买
+                    minPrice = Math.min(minPrice, dfs(price, special, nxtNeeds, filterSpecial, n) + specialPrice);
+                }
+            }
+            memo.put(curNeeds, minPrice);
+        }
+        return memo.get(curNeeds);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
