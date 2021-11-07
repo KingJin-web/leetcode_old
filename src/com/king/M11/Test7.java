0fa8b7fc6a0b2da68e7536cb643d5ac202bd0aeb
KingJin-web <1261337209@qq.com>
2021-11-07 23:44:50 +0800

    ✔ 598. 范围求和 II

---
+++ src/com/king/M11/Test7.java
@@ -0,0 +1,25 @@
+package com.king.M11;
+
+/**
+ * @program: leetcode
+ * @description: 598. 范围求和 II
+ * @author: King
+ * @create: 2021-11-07 23:43
+ */
+public class Test7 {
+
+    //执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+    //内存消耗： 38.4 MB , 在所有 Java 提交中击败了 66.72% 的用户
+    public int maxCount(int m, int n, int[][] ops) {
+        if(ops.length==0){
+            return m*n;
+        }
+        int a=Integer.MAX_VALUE;
+        int b=Integer.MAX_VALUE;
+        for(int i=0;i<ops.length;i++){
+            a=Math.min(a,ops[i][0]);
+            b=Math.min(b,ops[i][1]);
+        }
+        return a*b;
+    }
+}
