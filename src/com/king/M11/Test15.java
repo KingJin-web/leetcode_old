a96c616b525e5dfc0760433b21321be6e284404d
KingJin-web <1261337209@qq.com>
2021-11-16 23:59:43 +0800

    ✔  319. 灯泡开关

--- src/com/king/M11/Test15.java
+++ src/com/king/M11/Test15.java
@@ -39,3 +39,60 @@ public class Test15 {
         MyPrint.print(test15.bulbSwitch(3));
     }
 }
+class Solution {
+    public boolean isRectangleCover(int[][] r) {
+        //只有4个奇数次出现的坐标/
+        // 面积相等
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MIN_VALUE;
+        int top = Integer.MIN_VALUE;
+        int bottom = Integer.MAX_VALUE;
+        int n = r.length;
+
+        Set<String>set = new HashSet<>();
+        int sum = 0; // 小矩形相加
+        for(int i  =  0; i < n; i++){
+            int []t = r[i];
+            //更新大矩形四个点坐标
+            left = Math.min(left, t[0]);
+            bottom = Math.min(bottom, t[1]);
+            right = Math.max(right, t[2]);
+            top = Math.max(top, t[3]);
+            //计算小矩形面积
+            sum += (caculateArea(t[0], t[1], t[2], t[3]));
+            //记录坐标
+            String [] strings = new String[4];
+            // 小矩形四个点坐标
+            strings[0] = merge(t[0], t[1]);
+            strings[1] = merge(t[0], t[3]);
+            strings[2] = merge(t[2], t[1]);
+            strings[3] = merge(t[2], t[3]);
+            // 出现偶数次的抵消，奇数次保留
+            for(int j = 0; j < 4; j++){
+                if(set.contains(strings[j]))set.remove(strings[j]);
+                else set.add(strings[j]);
+            }
+        }
+        //最后必须只剩4个点并且set中的4个点与大矩形4个点坐标对应才表示位置匹配
+        if(set.size() == 4 &&
+                set.contains(merge(left, top)) &&
+                set.contains(merge(left, bottom)) &&
+                set.contains(merge(right, top))&&
+                set.contains(merge(right, bottom))
+        ){
+            return sum == caculateArea(left, bottom, right, top);// 面积相等即可
+        }
+        return false;
+    }
+    int caculateArea(int left, int bottom, int right, int top){
+        return (top - bottom) * (right - left);
+    }
+    String merge(int a, int b){
+        return a + " " + b;
+    }
+}
+
+作者：LittleSongFly
+        链接：https://leetcode-cn.com/problems/perfect-rectangle/solution/xiao-song-man-bu-ji-suan-wei-zhi-yu-mian-ymi7/
+        来源：力扣（LeetCode）
+        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
