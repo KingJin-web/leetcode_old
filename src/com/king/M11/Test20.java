ef5ac03a4c2f31ce88d91f1e365cadc77ecb9bd1
KingJin-web <1261337209@qq.com>
2021-11-21 22:24:33 +0800

    ✔ 594. 最长和谐子序列

--- src/com/king/M11/Test20.java
+++ src/com/king/M11/Test20.java
@@ -4,6 +4,8 @@ import com.king.util.LeetcodeUtil;
 import com.king.util.MyPrint;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @program: leetcode
@@ -23,7 +25,7 @@ public class Test20 {
     //数组的子序列是一个由数组派生出来的序列，它可以通过删除一些元素或不删除元素、且不改变其余元素的顺序而得到。
 
 
-    public int findLHS(int[] nums) {
+    public int findLHS1(int[] nums) {
         Arrays.sort(nums);
         int begin = 0, res = 0;
         for (int end = 0; end < nums.length; end++) {
@@ -35,6 +37,37 @@ public class Test20 {
         return res;
     }
 
+    //执行用时： 18 ms , 在所有 Java 提交中击败了 43.71% 的用户 内存消耗： 40.1 MB , 在所有 Java 提交中击败了 5.89% 的用户
+    public int findLHS2(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int res = 0;
+        for(int num : map.keySet()){
+            if(map.containsKey(num - 1))
+                res = Math.max(res, map.get(num - 1) + map.get(num));
+            if(map.containsKey(num + 1))
+                res = Math.max(res, map.get(num + 1) + map.get(num));
+        }
+        return res;
+
+    }
+    public int findLHS(int[] nums) {
+        HashMap <Integer, Integer> cnt = new HashMap <>();
+        int res = 0;
+        for (int num : nums) {
+            cnt.put(num, cnt.getOrDefault(num, 0) + 1);
+        }
+        for (int key : cnt.keySet()) {
+            if (cnt.containsKey(key + 1)) {
+                res = Math.max(res, cnt.get(key) + cnt.get(key + 1));
+            }
+        }
+        return res;
+
+    }
     public static void main(String[] args) {
         //输入：nums = [1,3,2,2,5,2,3,7]
         //输出：5
@@ -43,5 +76,10 @@ public class Test20 {
         Test20 test20 = new Test20();
         int a = test20.findLHS(LeetcodeUtil.stringToIntegerArray("[1,3,2,2,5,2,3,7]"));
         MyPrint.print(a);
+
+        short aa = -20;
+
+        int x = 2, y = 4;
+        System.out.println(++x != y--);
     }
 }
---
+++ src/com/king/M11/Test21.java
@@ -0,0 +1,10 @@
+package com.king.M11;
+
+/**
+ * @program: leetcode
+ * @description:
+ * @author: King
+ * @create: 2021-11-21 21:05
+ */
+public class Test21 {
+}
