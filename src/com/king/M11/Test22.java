db8d99133f40e3743aacea3f2e9679f4a9904e5d
KingJin-web <1261337209@qq.com>
2021-11-22 23:16:12 +0800

    ✔ 384. 打乱数组

---
+++ src/com/king/M11/Test22.java
@@ -0,0 +1,31 @@
+package com.king.M11;
+
+/**
+ * @program: leetcode
+ * @description: 384. ด๒ยาสýื้
+ * @author: King
+ * @create: 2021-11-22 22:56
+ */
+public class Test22 {
+    class Solution {
+
+        public Solution(int[] nums) {
+
+        }
+
+        public int[] reset() {
+
+        }
+
+        public int[] shuffle() {
+
+        }
+    }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
+}
