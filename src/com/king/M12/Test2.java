4f154277f037e2fa14dd64bc20dc129733edd755
KingJin-web <1261337209@qq.com>
2021-12-02 16:43:52 +0800

    ✔ 506. 相对名次

--- src/com/king/M12/Test2.java
+++ src/com/king/M12/Test2.java
@@ -1,10 +1,13 @@
 package com.king.M12;
 
 import MyJava.exam.T2;
+import com.king.Helper;
 import com.king.util.LeetcodeUtil;
 import com.king.util.MyPrint;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @program: leetcode
@@ -27,7 +30,7 @@ public class Test2 {
     //从名次第 4 到第 n 的运动员，只能获得他们的名次编号（即，名次第 x 的运动员获得编号 "x"）。
     //使用长度为 n 的数组 answer 返回获奖，其中 answer[i] 是第 i 位运动员的获奖情况。
 
-    public String[] findRelativeRanks(int[] score) {
+    public String[] findRelativeRanks1(int[] score) {
         int n = score.length;
         String[] desc = {"Gold Medal", "Silver Medal", "Bronze Medal"};
         int[][] arr = new int[n][2];
@@ -54,7 +57,45 @@ public class Test2 {
         //解释：名次为 [1st, 2nd, 3rd, 4th, 5th] 。
         Test2 test2 = new Test2();
         MyPrint.print(test2.findRelativeRanks(LeetcodeUtil.stringToIntegerArray("[5,4,3,2,1]")));
+        //["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+        MyPrint.print(test2.findRelativeRanks(LeetcodeUtil.stringToIntegerArray("[10,3,8,9,4]")));
+    }
 
+    //执行用时： 177 ms , 在所有 Java 提交中击败了 5.07% 的用户 内存消耗： 39.4 MB , 在所有 Java 提交中击败了 68.51% 的用户
+    public String[] findRelativeRanks(int[] nums) {
+        int n = nums.length;
+        String[] desc = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        sort(nums);
+        String[] strings = new String[n];
+
+//        MyPrint.print(list);
+//        MyPrint.print(nums);
+        for (int i = 0; i < n; i++) {
+            if (i < 3){
+                strings[list.indexOf(nums[i])] = desc[i];
+            }else {
+                strings[list.indexOf(nums[i])] = Integer.toString(i + 1);
+            }
+        }
+
+        return strings;
+    }
+
+    public void sort(int[] arr) {
+        int temp;//定义一个临时变量
+        for (int i = 0; i < arr.length - 1; i++) {//冒泡趟数
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j + 1] > arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 
 }
