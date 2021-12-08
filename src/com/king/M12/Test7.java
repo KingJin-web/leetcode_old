e43823ad3d6fef953f92efaa15746fe743554ac7
KingJin-web <1261337209@qq.com>
2021-12-08 22:25:50 +0800

    ✔ 1816. 截断句子

--- src/com/king/M12/Test7.java
+++ src/com/king/M12/Test7.java
@@ -1,6 +1,11 @@
 package com.king.M12;
 
+import com.king.Helper;
 import com.king.util.MyPrint;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @program: leetcode
@@ -40,11 +45,39 @@ public class Test7 {
         return grid;
     }
 
+
+    public int MAX(int a, int b) {
+        return a > b ? a : b;
+    }
+
+
+    public void deleteList(List<Integer> list) {
+
+        double sum = 0;
+        for (int num : list) {
+            sum += num;
+        }
+        double avg = sum / list.size();
+        List<Integer> temp = new ArrayList<>();
+        for (int num : list) {
+            if (num < avg) {
+                temp.add(num);
+            }
+        }
+
+        list.removeAll(temp);
+    }
+
+
     public static void main(String[] args) {
         //ÊäÈë£ºgrid = [[1,2,2],[2,3,2]], row = 0, col = 1, color = 3
         //Êä³ö£º[[1,3,3],[2,3,3]]
         int arr[][] = {{1, 2, 2}, {2, 3, 2}};
         Test7 test7 = new Test7();
+        List<Integer> list = new ArrayList<>();
+        list.addAll(Helper.getList(1,2,3,4,5,6));
+        test7.deleteList(list);
+        MyPrint.print(list);
 
         MyPrint.print(test7.colorBorder(arr, 0, 1, 3));
 
@@ -52,11 +85,8 @@ public class Test7 {
         int a = 2, b = 3;
         int c = Math.max(a, b);
         System.out.println(c);
-        c = test7.MAX(a,b);
+        c = test7.MAX(a, b);
         System.out.println(c);
     }
 
-    public int MAX(int a, int b) {
-        return a > b ? a : b;
-    }
 }
