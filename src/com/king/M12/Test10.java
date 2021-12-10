157416dd92c6daf78631561b92903f4960aeb671
KingJin-web <1261337209@qq.com>
2021-12-10 23:39:21 +0800

    ✔ 748. 最短补全词

---
+++ src/com/king/M12/Test10.java
@@ -0,0 +1,11 @@
+package com.king.M12;
+
+/**
+ * @program: leetcode
+ * @description: 748. ×î¶Ì²¹È«´Ê
+ * https://leetcode-cn.com/problems/shortest-completing-word/
+ * @author: King
+ * @create: 2021-12-10 23:09
+ */
+public class Test10 {
+}
