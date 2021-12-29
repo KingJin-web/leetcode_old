682eb2b7e21d1428c32593b9fe3de66617e175b3
KingJin-web <1261337209@qq.com>
2021-12-29 23:18:23 +0800

    ✔ 1995. 统计特殊四元组

---
+++ src/com/king/M12/Test29.java
@@ -0,0 +1,18 @@
+package com.king.M12;
+
+/**
+ * @program: leetcode
+ * @description:
+ * @author: King
+ * @create: 2021-12-29 20:35
+ */
+public class Test29 {
+    //1995. 统计特殊四元组
+    //给你一个 下标从 0 开始 的整数数组 nums ，返回满足下述条件的 不同 四元组 (a, b, c, d) 的 数目 ：
+    //
+    //nums[a] + nums[b] + nums[c] == nums[d] ，且
+    //a < b < c < d
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
